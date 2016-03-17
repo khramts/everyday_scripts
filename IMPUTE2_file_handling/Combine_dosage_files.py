25ef3ecb858481ee1526508f487d4708eeda5f50
Katya Khramtsova <eakhram@uchicago.edu>
2016-03-17 14:43:24 -0500

    Initial commit

---
+++ IMPUTE2_file_handling/Combine_dosage_files.py
@@ -0,0 +1,26 @@
+'''
+author: Katya
+date: 03/17/2016
+
+This script is for combining dosage files from two data sets which have been previously imputed separately.
+
+# Dosage file format
+# Rs_number A1 A2 FID1 FID2 FID2 IID2 ... FIDn IIDn
+# rs146752890 C G 0.791 0.206 0.946 0.053
+# In this file format two dosages are given: A1A1 and A1A2. A2A2 can be calculated by substracting the other two dosages from 1.
+
+# Map file format
+# Chromosome Variant_identifier(rs_number) Position(in_morgans_or_centimorgans) Base-pair_coordinate
+# 22 rs7290642 0 27001364
+
+# Fam file format
+# FID IID Father_ID Mother_ID Sex Phenotype
+#
+
+# Step 1:
+'''
+
+
+
+
+
